cf75916cf7d7bfbd1c9b9b9e5a795ec379c5f69a
Rcream <shreyasjobyk@gmail.com>
2026-06-09 11:27:36 +1000

    v6 - 2
    - changed money formatting

--- script.js
+++ script.js
@@ -166,9 +166,10 @@ function resolveRarity(odds) {
 }
 
 function formatMoney(n) {
-  if (n >= 1e6) return '$' + (n / 1e6).toFixed(2) + 'M';
-  if (n >= 1e3) return '$' + (n / 1e3).toFixed(1) + 'K';
-  return '$' + Math.floor(n);
+  if (n < 1000) return '$' + Math.floor(n);
+  if (n < 1e6) return '$' + (n / 1e3).toFixed(1) + 'K';
+  if (n < 1e9) return '$' + (n / 1e6).toFixed(1) + 'M';
+  return '$' + (n / 1e9).toFixed(2) + 'B';
 }
 
 function capitalize(s) {
